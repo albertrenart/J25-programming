ef5edb980898c9fe5bb0d4e527f473fda5f7fb21
albertrenart <144990839+albertrenart@users.noreply.github.com>
2023-11-09 08:24:35 +0100

    Add files via upload

---
+++ arduino/task_special_character.ino
@@ -0,0 +1,36 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+Serial.println("________$$$$..");
+Serial.println("______$$$$$$$$$");
+Serial.println("______$$$$$$$_$");
+Serial.println("_____$$$$$$$$$$");
+Serial.println("______$$$$$$$$$$");
+Serial.println("_____$$$$$$_$$$$$");
+Serial.println("____$$$$$$$_____$$$");
+Serial.println("____$$$$$$$$_____$");
+Serial.println("____$$$$$$$$$$");
+Serial.println("_____$$$$$$$$$$");
+Serial.println("_____$$$$$$$$$$$");
+Serial.println("______$$$$$$$$$$$");
+Serial.println("_$$$$___$$$$$$$$$");
+Serial.println("__$$$$$$$$$$$$$$$");
+Serial.println("_$$$$$$$$$$$$$$$");
+Serial.println("__$$$$$$$$$$$$$");
+Serial.println("$$$$$$$$$$$$$");
+Serial.println("__$__$$$$$$");
+Serial.println("____$$$$$$");
+Serial.println("____$$$$$");
+Serial.println("___$$$$$$_____$");
+Serial.println("___$$$$$$___$$_$$");
+Serial.println("____$$$$$___$__$$");
+Serial.println("____$$$$$______$$");
+Serial.println("_____$$$$$____$$$");
+Serial.println("_______$$$$$$$$$");
+Serial.println("__________$$$$");
+Serial.println("\n\n\n\n\n\n\n\n\n\n");
+delay(5000);
+
+}
