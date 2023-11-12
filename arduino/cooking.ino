90c916695669625783dff926f57338697d7a600c
albertrenart <144990839+albertrenart@users.noreply.github.com>
2023-11-12 23:28:07 +0100

    Update cooking.ino

--- arduino/cooking.ino
+++ arduino/cooking.ino
@@ -1,3 +1,11 @@
+//This program simulates that it is cooking.
+//In the setup first we initiate the Serial Monitor.
+//In the loop, the code is continuously reading the input ( String input = Serial.readString(); )
+//Then we have the first 'if' of the program, its purpose is to only send the input to the function 'decide' if there is something written.
+//The purpose of the 'decide' function is to call the 'cook' function only if the input is 'cook'.
+//The function cook only prints a sentence: Serial.println("we are cooking");
+
+
 void setup() {
   Serial.begin (9600);
 }
