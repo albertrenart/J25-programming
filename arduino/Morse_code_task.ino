8ea0b572f212964a8c041ae2b4593cc750823660
albertrenart <144990839+albertrenart@users.noreply.github.com>
2023-11-12 23:07:46 +0100

    Update Morse_code_task.ino

--- arduino/Morse_code_task.ino
+++ arduino/Morse_code_task.ino
@@ -1,4 +1,5 @@
-//This program first sets the output. It is designed to flash my name in morse code in the builtin LED of the arduino. The Morse code for each letter is represented by a sequence of dots (short flashes) and dashes (long flashes).
+//This program first sets the output. It is designed to flash my name in Morse code in the built-in LED of the Arduino. 
+//The Morse code for each letter is represented by a sequence of dots (short flashes) and dashes (long flashes).
 
 
 
