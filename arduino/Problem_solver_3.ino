5ab648984ed92d99db8ccac8b11be5126cebd7d1
albertrenart <144990839+albertrenart@users.noreply.github.com>
2023-11-21 10:10:36 +0100

    Update Problem_solver_3.ino

--- arduino/Problem_solver_3.ino
+++ arduino/Problem_solver_3.ino
@@ -16,6 +16,22 @@ void setup() {
   }
 
 
+//solves only the ones that end with 6 or 3
+void setup() {
+  Serial.begin(9600);
+    for (int numeroProblema = 1; numeroProblema <= 100; ++numeroProblema) {
+        if (numeroProblema % 10 == 3 || numeroProblema % 10 == 6) {
+            resolverProblemaDibujo(numeroProblema);
+        }
+    }
+}
+  void loop() {
+  }
+
+  void resolverProblemaDibujo(int numeroProblema) { 
+    Serial.print("Resolviendo problema");
+    Serial.println(numeroProblema);
+  }
 
 
     
