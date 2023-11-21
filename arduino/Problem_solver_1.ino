b7d2e004ea77da7bc32f90cd05e9928c8b16cdb3
albertrenart <144990839+albertrenart@users.noreply.github.com>
2023-11-21 10:19:57 +0100

    Update Problem_solver_1.ino

--- arduino/Problem_solver_1.ino
+++ arduino/Problem_solver_1.ino
@@ -1,9 +1,8 @@
 void setup() {
 Serial.begin(9600);
 for(int numeroProblema= 1;numeroProblema<=30;numeroProblema=numeroProblema+2){
-resolverProblemaDibujo(numeroProblema); //solo problemas impares
-}
-//cambio de orden
+resolverProblemaDibujo(numeroProblema); 
+
 for(int numeroProblema=30;numeroProblema>=1;numeroProblema--){
 resolverProblemaDibujo(numeroProblema);
 }
@@ -13,7 +12,7 @@ void loop() {
 
 
 }
-void resolverProblemaDibujo(int n){ //data type, number
+void resolverProblemaDibujo(int n){ 
 	Serial.print("Resolviendo problema");
 	Serial.println(n);
 }
