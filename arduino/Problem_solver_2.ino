783325a6795477090dbe39fde01ce86143396fb7
albertrenart <144990839+albertrenart@users.noreply.github.com>
2023-11-21 09:34:27 +0100

    Create Problem_solver_2.ino

---
+++ arduino/Problem_solver_2.ino
@@ -0,0 +1,16 @@
+//Todos menos el 17 
+void setup() {
+  Serial.begin(9600);
+  for (int numeroProblema = 1; numeroProblema <= 30; numeroProblema++) {
+    if (numeroProblema != 17) {
+      resolverProblemaDibujo(numeroProblema);
+    }
+  }
+}
+  void loop() {
+  }
+
+  void resolverProblemaDibujo(int n) {  //data type, number
+    Serial.print("Resolviendo problema");
+    Serial.println(n);
+  }
