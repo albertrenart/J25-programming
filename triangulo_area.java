85a0fb2b61605571d32a04002038efcfa68767d6
albertrenart <144990839+albertrenart@users.noreply.github.com>
2024-02-05 15:25:06 +0100

    Create triangulo_area.java

---
+++ triangulo_area.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+	double base, area, height; 
+	base = 2.5; 
+  height = 2.5;
+    area = base * height / 2;
+
+    System.out.print ("Area = "); 
+    System.out.print (area); 
+  }
+}
+
