13fdfbc1117cd76a5e4e446a29863ab5e696b94d
albertrenart <144990839+albertrenart@users.noreply.github.com>
2024-02-05 16:06:20 +0100

    Update ejercicio 1.java

--- programming (java)/ejercicio 1.java
+++ programming (java)/ejercicio 1.java
@@ -1,4 +1,4 @@
-public class ArithmeticTest {     
+public class Ejercicio 1 {     
    public static void main(String[] args) {
     int number1 = 98;
      int number2 = 5;
@@ -8,12 +8,12 @@ public class ArithmeticTest {
       product = number1 * number2;
      quotient = number1 / number2;
       remainder = number1 % number2;
-      System.out.println("The sum, difference, product, quotient and remainder of " + number1 + "and" + sum + "," + difference + "," + product + "," + quotient + ", and" + remainder);
+      System.out.println("Sum, difference, product, quotient and remainder of " + number1 + "and" + sum + "," + difference + "," + product + "," + quotient + ", and" + remainder);
       ++number1;
     --number2;
-      System.out.println("number1 after increment is " + number1);  
-      System.out.println("number2 after decrement is " + number2);
+      System.out.println("number1 after increment: " + number1);  
+      System.out.println("number2 after decrement: " + number2);
         quotient = number1 / number2; 
-      System.out.println("The new quotient of " + number1 + " and " + number2 + " is " + quotient);
+      System.out.println("New quotient of " + number1 + " and " + number2 + " : " + quotient);
    }
 }
