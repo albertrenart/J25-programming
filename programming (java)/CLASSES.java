1e469f02fea700049e594fe6b2bf92301e3a6b1c
albertrenart <144990839+albertrenart@users.noreply.github.com>
2024-03-04 13:41:24 +0100

    Create CLASSES.java

---
+++ programming (java)/CLASSES.java
@@ -0,0 +1,30 @@
+// getters 
+public int getBiggerSide(){
+  return this.biggerSide;
+}
+
+public int getSmallerSide() {
+  return this.smallerSide;
+}
+
+public String getColour() {
+  return this.colour;
+}
+
+public int getHeight() {
+  return this.height;
+}
+
+//setters
+
+public void setBiggerSide (int biggerSide) {
+  this.biggerSide = biggerSide;
+} 
+
+public void setSmallerSide (int smallerSide) {
+  this.smallerSide = smallerSide;
+}
+
+public void setHeight (int height) {
+  this.height = height;
+}
