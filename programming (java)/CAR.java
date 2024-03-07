bc8d4eca4f8d71581b5ee56ee0c8f16b529ea692
albertrenart <144990839+albertrenart@users.noreply.github.com>
2024-03-07 09:40:31 +0100

    Create CAR.java

---
+++ programming (java)/CAR.java
@@ -0,0 +1,21 @@
+// Lending store
+
+double: price
+boolean: promotion
+String: brand
+boolean: secondhand
+String[]: extras
+String: category
+int: consumption
+int: power
+String: colour
+
+
+// Car workshop
+
+
+String: plate
+Client: owner
+String: problems
+String: brand
+String: model
