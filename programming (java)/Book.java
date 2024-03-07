b2e9812e7c9d058fe3ce8ef4fd7ff492a0d9c7f3
albertrenart <144990839+albertrenart@users.noreply.github.com>
2024-03-07 08:58:25 +0100

    Create Book.java

---
+++ programming (java)/Book.java
@@ -0,0 +1,34 @@
+// Context of library
+
+
+  String: title
+  String: author
+  String: ISBN
+  String: genre
+  boolean: lended
+  Lendinggs[]: lendingHistory
+  Library: location
+
+
+// Context of bookstore
+
+
+  String: title
+  String: author
+  String: ISBN
+  String: genre
+  double: price 
+  boolean: promotion
+  String: section
+
+
+  // Context of Minecraft
+
+
+  Point: position
+  String: title
+  String: content
+  MinecraftUser: author
+  int: pages
+
+  
