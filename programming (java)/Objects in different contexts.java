0464ae6bba15dc4c5e176bbb3a163f0767074a9e
albertrenart <144990839+albertrenart@users.noreply.github.com>
2024-03-11 23:37:10 +0100

    Create Objects in different contexts.java

---
+++ programming (java)/Objects in different contexts.java
@@ -0,0 +1,37 @@
+Object: Car           // when we are buying a car in different contexts what do we look for?
+
+DalilyUseCar
+int seats
+int fuelOrEnergyConsumption  // in l/100 km or kWh/100 km if its electric
+String brand
+String model
+boolean electric
+boolean hybrid
+int previousOwners
+int price
+
+
+RacingCar
+int maxSpeed   // in km/h
+int zeroToHundred   // in seconds
+String wheelDrive    // rear, front, all-wheel
+int HundredToZero   // in meters
+int price
+int weight
+double power
+
+
+OffRoad
+boolean allWheelDrive
+double torque
+int wheels           // number of wheels
+String typeOfWheels
+String transmision   // type of transmision
+int differential
+boolean differentialLock
+int attackAngle
+int exitAngle
+
+
+
+
